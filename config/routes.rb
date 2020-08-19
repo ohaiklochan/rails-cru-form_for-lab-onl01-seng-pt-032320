f08c4ad6b8116a2f20bc7751fde6fa83833a41ac
Chloe Steffens <caforonda103@yahoo.com>
2020-08-19 14:45:41 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'songs/create'
+
+  get 'songs/update'
+
+  get 'songs/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
