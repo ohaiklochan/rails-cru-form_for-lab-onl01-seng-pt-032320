e865fe7acfbf34a16dcb3f6f1d44718affe94c9a
Chloe Steffens <caforonda103@yahoo.com>
2020-08-19 14:56:05 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,10 @@ class SongsController < ApplicationController
 
   def show
   end
-  
+
+  def index
+  end
+
   def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
   get 'songs/new'
 
   get 'songs/edit'
