642cf83ff56dc0478feb2ee2206ec1493cef2b44
Chloe Steffens <caforonda103@yahoo.com>
2020-08-19 15:09:57 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,6 @@
 Rails.application.routes.draw do
-  get 'genres/index'
-
-  get 'genres/update'
-
-  get 'artists/index'
-
-  get 'artists/update'
-
-  get 'songs/index'
-
-  get 'songs/new'
-
-  get 'songs/edit'
-
-  get 'genres/new'
-
-  get 'genres/create'
-
-  get 'genres/edit'
-
-  get 'genres/show'
-
-  get 'artists/new'
-
-  get 'artists/create'
-
-  get 'artists/edit'
-
-  get 'artists/show'
-
-  get 'songs/create'
-
-  get 'songs/update'
-
-  get 'songs/show'
-
+  resources :songs
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
